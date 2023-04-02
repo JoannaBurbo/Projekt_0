a44d2155a8f54241963ef86d6691074b84d6eb8b
JoannaBurbo <joannamezyk@gmail.com>
2023-04-02 15:56:45 +0200

    Test1

---
+++ src/test/java/UltimateQATest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class UltimateQATest extends PageSetup{
+
+
+    @Test
+    public void testOne() {
+        WebElement buttonUsingId = driver.findElement(By.id("idExample"));
+        buttonUsingId.click();
+        WebElement buttonSuccessText = driver.findElement(By.className("entry-title"));
+        Assertions.assertEquals("Button success", buttonSuccessText.getText());
+    }
+    @Test
+    public void testTwo() throws InterruptedException {
+        WebElement nameInput = driver.findElement(By.id("et_pb_contact_name_0"));
+        nameInput.sendKeys("Tester");
+
+        WebElement emailInput = driver.findElement(By.id("et_pb_contact_email_0"));
+        emailInput.sendKeys("tester@tester.pl");
+
+        Thread.sleep(2000);
+
+        WebElement emailMebutton = driver.findElement(By.name("et_builder_submit_button"));
+        emailMebutton.click();
+
+        Thread.sleep(2000);
+        WebElement thanksText =driver.findElement(By.xpath("//div[@class='et-pb-contact-message']/p"));
+        Assertions.assertEquals("Thanks for contacting us",thanksText.getText());
+    }
+
+}
